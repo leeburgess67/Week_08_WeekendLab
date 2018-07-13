3d8709118bfc723ebabad368bfed9950bd8ca179
leeburgess67 <leeburgessmail@gmail.com>
2018-07-13 15:45:49 +0100

    Player test set up

--- src/test/java/CompetitionTest.java
+++ src/test/java/CompetitionTest.java
@@ -1,2 +1,46 @@
+import models.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class CompetitionTest {
+
+    private Team celtic;
+    private Team barca;
+    private List<Player> players;
+    private Manager rodgers;
+    private int points;
+    private List<Competition> competitions;
+    private Competition champ;
+    private Player player1;
+
+    public CompetitionTest() {
+    }
+
+
+    @Before
+    public void before(){
+        players = new ArrayList<Player>();
+        player1 = new Player("New Player", PositionType.STRIKER, celtic);
+        players.add(player1);
+        rodgers = new Manager("Brendan Rodgers");
+        celtic = new Team("Celtic", rodgers, 50);
+        barca = new Team("Barca", rodgers, 50);
+        List<Team> teams = new ArrayList<Team>();
+        teams.add(barca);
+        teams.add(celtic);
+        champ = new Competition(CompetitionType.CHAMPIONS_LEAGUE,teams);
+    }
+
+    @Test
+    public void canGetCompetitionType(){
+        assertEquals(CompetitionType.CHAMPIONS_LEAGUE, champ.getCompetition());
+    }
+
+
+
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,7 +1,40 @@
-//import org.junit.Test;
-//
-//public class PlayerTest {
-//
-//    @Test
-//
-//}
+import models.Manager;
+import models.Player;
+import models.PositionType;
+import models.Team;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
+public class PlayerTest {
+
+    Player brown;
+    Team celtic;
+    PositionType midfield;
+    Manager rodgers;
+
+    @Before
+    public void before(){
+        rodgers = new Manager("Brendan Rodgers");
+        celtic = new Team("Celtic", rodgers, 50);
+        brown = new Player("Scott Brown", PositionType.DEFENSIVE_MIDFIELDER, celtic);
+
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Scott Brown", brown.getName());
+    }
+
+    @Test
+    public void canGetPosition(){
+        assertEquals(PositionType.DEFENSIVE_MIDFIELDER, brown.getPosition());
+    }
+
+    @Test
+    public void canGetTeam(){
+        assertEquals(celtic, brown.getTeam());
+    }
+}
