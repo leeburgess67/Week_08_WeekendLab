bdfea85655aa41d047e6d7cae3bc9064815470f2
leeburgess67 <leeburgessmail@gmail.com>
2018-07-13 12:10:56 +0100

    Postition Type Enum set up

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,4 +1,15 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "players")
 public class Player {
+
+    private int id;
+    private String name;
+    private
+
+
 }
---
+++ src/main/java/models/PositionType.java
@@ -0,0 +1,16 @@
+package models;
+
+import javax.persistence.Column;
+
+public enum PositionType {
+    LEFT_BACK,
+    CENTRE_BACK,
+    RIGHT_BACK,
+    LEFT_WING,
+    DEFENSIVE_MIDFIELDER,
+    ATTACKING_MIDFIELDER,
+    RIGHT_WING,
+    STRIKER
+}
+
+
