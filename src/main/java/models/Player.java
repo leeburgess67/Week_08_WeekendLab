68b610731d847192719568050c27604638e271e1
leeburgess67 <leeburgessmail@gmail.com>
2018-07-13 12:15:49 +0100

    Player Class set up

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,7 +1,6 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "players")
@@ -9,7 +8,52 @@ public class Player {
 
     private int id;
     private String name;
-    private
+    private PositionType position;
+    private Team team;
 
+    public Player(){}
 
+    public Player(String name, PositionType position, Team team) {
+        this.name = name;
+        this.position = position;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "position")
+    public PositionType getPosition() {
+        return position;
+    }
+
+    public void setPosition(PositionType position) {
+        this.position = position;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
