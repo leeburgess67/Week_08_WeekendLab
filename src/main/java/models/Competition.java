c2ddba221f3a49c581d0c6deb788168b373b4d9f
leeburgess67 <leeburgessmail@gmail.com>
2018-07-13 12:27:42 +0100

    Competition classes set up

--- src/main/java/models/Competition.java
+++ src/main/java/models/Competition.java
@@ -1,12 +1,56 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.sun.tools.javah.Gen;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "competions")
 public class Competition {
 
     private int id;
-    private
+    private CompetitionType competition;
+    private List<Team> teams;
+
+    public Competition(CompetitionType competition, List<Team> teams) {
+        this.competition = competition;
+        this.teams = teams;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "competitions")
+    public CompetitionType getCompetition() {
+        return competition;
+    }
+
+    public void setCompetition(CompetitionType competition) {
+        this.competition = competition;
+    }
+
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @ManyToMany
+    @JoinTable(name = "teams_competitions",
+            joinColumns = {@JoinColumn(name = "competition_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "team_id", nullable = false, updatable = false)})
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
 }
+
+
